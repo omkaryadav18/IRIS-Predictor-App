dc23d241c121cb5108d817624de5c104694f3d52
Omkar Yadav <omkaryadav1812@gmail.com>
2025-10-06 13:26:30 +0530

    Added all the files

---
+++ predictor/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import home, predict_view, predict_api
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("predict/", predict_view, name="predict"),
+    path("api/predict/", predict_api, name="predict_api"),
+]
